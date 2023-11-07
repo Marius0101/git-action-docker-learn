8714ac1c056b77ac18a6293016642ef53426af95
Marius Mihalcea <mariusoctavian010@gmail.com>
2023-11-07 12:05:43 +0200

    try add repo url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -l
 
 echo "Hello $1"
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+    echo "::debug::The even name was '$GITHUB_EVENT_NAME'"
+    exit 0 
+fi
+
+even_type = $(jq --raw-output .action $GITHUB_EVENT_PATH)
+if [ $even_type != "closed"]; then
+    echo "::debug::The even type was '$even_type'"
+    exit 0 
+fi
+milestone_name = $(jq --raw-output .milestion.title $GITHUB_EVENT_PATH)
+
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
