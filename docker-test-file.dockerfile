642e3057d6eef6329d200a815df9825d40a106bf
Marius Mihalcea <mariusoctavian010@gmail.com>
2023-11-07 12:49:46 +0200

    add githbu release

--- docker-test-file.dockerfile
+++ docker-test-file.dockerfile
@@ -5,6 +5,9 @@ FROM mcr.microsoft.com/dotnet/sdk:6.0
 COPY entrypoint.sh /entrypoint.sh
 
 RUN apt-get update && apt-get install -y jq
+RUN dotnet tool install -g GitReleaseManger.Tool
 
+ENV PATH /root/.dotnet/tools:$PATH
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
+COPY  entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
+repo-token= $2
+
+
 if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
     echo "::debug::The even name was '$GITHUB_EVENT_NAME'"
     exit 0 
@@ -15,4 +18,17 @@ if [ $even_type != "closed"]; then
     exit 0 
 fi
 milestone_name = $(jq --raw-output .milestion.title $GITHUB_EVENT_PATH)
+
+IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
+
+release_url=$(dotnet gitreleasemanager create \
+--milestone $milestone_name \
+--targetcommitsh $GITHUB_SHA \
+--token $repo_token \
+--owner $owner \
+--repository $repository)
+
+
+echo "::set-output name = release-url::$release_url"
+exit 0
 
